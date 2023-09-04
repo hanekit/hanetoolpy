d347780002f1a9d7330b3b60a2dd8c33698e30af
hanekit <otonashihane@gmail.com>
2023-09-04 17:35:31 +0800

    update

--- src/hanetoolpy/about.py
+++ src/hanetoolpy/about.py
@@ -1,6 +1,6 @@
 program_name = "hanetoolpy"
-version_number = "0.0.1"
-build_date = "2023.08.08"
+version_number = "0.0.2"
+build_date = "2023.09.04"
 
 version_text = f"version {version_number}"
 build_text = f"build {build_date}"
