6c72a06dd9d86a9df052841e1598581a32f14633
hanekit <otonashihane@gmail.com>
2023-10-17 18:23:02 +0800

    Split

---
+++ src/hanetoolpy/typer/add_commands.py
@@ -0,0 +1,41 @@
+import typer
+
+
+def add_commands(parent):
+    # 添加 vasp 命令
+    vasp = typer.Typer(no_args_is_help=True,
+                       invoke_without_command=True)
+    parent.add_typer(vasp, name="vasp", help="VASP tools")
+
+    # 添加 vasp 子命令
+    from hanetoolpy.functions.vasp_run import vasp_run
+    vasp.command("run")(vasp_run)
+    from hanetoolpy.functions.vasp import check_vasp_end
+    vasp.command("check_end")(check_vasp_end)
+    from hanetoolpy.functions.vasp_stop import vasp_stop
+    vasp.command("stop", no_args_is_help=True)(vasp_stop)
+    from hanetoolpy.functions.global_band_plotter import plot as f101
+    vasp.command("f101")(f101)
+    from hanetoolpy.functions.ebs_unfold_plotter import main as f102
+    vasp.command("f102")(f102)
+
+    # 添加 vaspkit 命令
+    vaspkit = typer.Typer(no_args_is_help=True,
+                          invoke_without_command=True)
+    parent.add_typer(vaspkit, name="vaspkit", help="VASPKIT tools")
+
+    # 添加 vaspkit 子命令
+    from hanetoolpy.functions.vaspkit import vaspkit_281
+    vaspkit.command("f281")(vaspkit_281)
+
+    # 添加 thirdorder 命令
+    thirdorder = typer.Typer(no_args_is_help=True,
+                             invoke_without_command=True)
+    parent.add_typer(thirdorder, name="thirdorder", help="thirdorder tools")
+
+    # 添加 thirdorder 子命令
+    from hanetoolpy.functions.thirdorder import main as thirdorder_f101
+    thirdorder.command("f101")(thirdorder_f101)
+    from hanetoolpy.functions.thirdorder import \
+        check_thirdorder_jobs as thirdorder_f102
+    thirdorder.command("f102")(thirdorder_f102)
--- src/hanetoolpy/typer/main.py
+++ src/hanetoolpy/typer/main.py
@@ -1,19 +1,18 @@
-# Standard library imports
+import logging
+import time
 from pathlib import Path
 
 import typer
-from typing_extensions import Annotated
-import logging
-from rich.logging import RichHandler
-
 from hanetoolpy.about import __version__
-
-
+from hanetoolpy.typer.add_commands import add_commands
+from rich.logging import RichHandler
+from typing_extensions import Annotated
 
 app = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]},  # 给帮助增加 -h 选项
                   add_completion=False,  # 去除默认参数选项
                   invoke_without_command=True  # 无子命令时运行 callback
                   )
+add_commands(app)
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s][%(levelname)-s]: %(message)s',
@@ -22,6 +21,7 @@ logging.basicConfig(level=logging.INFO,
                                           show_time=False,
                                           show_level=False,
                                           show_path=False)])
+logging.info("hanetoolpy start...")
 
 current_file_path = Path(__file__).resolve()
 package_root = current_file_path.parent.parent
@@ -42,45 +42,6 @@ def main(context: typer.Context,
         # no_args_is_help=False 才会执行
         print("use -h for help.")
 
-
-# 添加 vasp 命令
-vasp = typer.Typer(no_args_is_help=True,
-                   invoke_without_command=True)
-app.add_typer(vasp, name="vasp", help="VASP tools")
-
-# 添加 vasp 子命令
-from hanetoolpy.functions.vasp_run import vasp_run
-from hanetoolpy.functions.vasp import check_vasp_end
-from hanetoolpy.functions.vasp_stop import vasp_stop
-vasp.command("run")(vasp_run)
-vasp.command("check_end")(check_vasp_end)
-vasp.command("stop", no_args_is_help=True)(vasp_stop)
-
-from hanetoolpy.functions.global_band_plotter import plot as f101
-vasp.command("f101")(f101)
-
-from hanetoolpy.functions.ebs_unfold_plotter import main as f102
-vasp.command("f102")(f102)
-
-# 添加 vaspkit 命令
-vaspkit = typer.Typer(no_args_is_help=True,
-                      invoke_without_command=True)
-app.add_typer(vaspkit, name="vaspkit", help="VASPKIT tools")
-
-# 添加 vaspkit 子命令
-from hanetoolpy.functions.vaspkit import vaspkit_281
-vaspkit.command("f281")(vaspkit_281)
-
-# 添加 thirdorder 命令
-thirdorder = typer.Typer(no_args_is_help=True,
-                         invoke_without_command=True)
-app.add_typer(thirdorder, name="thirdorder", help="thirdorder tools")
-
-# 添加 thirdorder 子命令
-from hanetoolpy.functions.thirdorder import main as thirdorder_f101
-from hanetoolpy.functions.thirdorder import check_thirdorder_jobs as thirdorder_f102
-thirdorder.command("f101")(thirdorder_f101)
-thirdorder.command("f102")(thirdorder_f102)
 
 if __name__ == "__main__":
     app()
