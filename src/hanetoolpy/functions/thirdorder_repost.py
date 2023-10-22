12582515325b5743ce73f45bb2de0ff54fee866f
hanekit <otonashihane@gmail.com>
2023-10-22 17:01:55 +0800

    fix

--- src/hanetoolpy/functions/thirdorder_repost.py
+++ src/hanetoolpy/functions/thirdorder_repost.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 from subprocess import run
 from typing import Tuple
 
@@ -14,7 +15,7 @@ def sow(
                                                                   help="size of supercell")],
         cutoff: Annotated[str, typer.Argument(metavar="-INT|+FLOAT",
                                               help="negative integer (n-th) or positive float (Ang)")],
-        path: Annotated[str, typer.Argument()] = thirdorder_vasp_path):
+        thirdorder_vasp_path: Annotated[str, typer.Argument(metavar="PATH",)] = thirdorder_vasp_path):
     """
     run the thirdorder_vasp.py sow
 
@@ -32,10 +33,14 @@ def sow(
         logging.error(error)
         logging.error("NOTE: Please confirm that you have installed thirdorder correctly.")
         raise typer.Abort()
+    thirdorder_vasp_path = Path(thirdorder_vasp_path)
+    if not thirdorder_vasp_path.exists():
+        logging.error(f"Can not find the thirdorder_vasp.py at: \n {thirdorder_vasp_path}")
+        logging.error(f"Please put this file into the above path, or specify the file path by argument.")
+        raise typer.Abort()
     python_command = "python"
-    thirdorder_vasp_path = path
     supercell = " ".join(map(str, supercell))
-    command = f"{python_command} {thirdorder_vasp_path} {action} {supercell} {cutoff}"
+    command = f"{python_command} {thirdorder_vasp_path} sow {supercell} {cutoff}"
     run(command, shell=True)
 
 
