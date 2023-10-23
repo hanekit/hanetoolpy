0c442351f497aec64630f4237a31c5ac138ddbeb
hanekit <otonashihane@gmail.com>
2023-10-23 12:54:40 +0800

    update confirm in pbs

--- src/hanetoolpy/cui/confirm.py
+++ src/hanetoolpy/cui/confirm.py
@@ -1,17 +1,26 @@
+import os
 import pyinputplus as pyip
 
 
 def confirm(prompt, default=None):
     prompt = f"{prompt} (default:{default}) (y/n) : "
+    in_pbs = os.environ.get("PBS_O_WORKDIR")
+
+    if in_pbs != None:
+        print(prompt + "y (Detected in pbs process, automatically confirmed.)")
+        return True
+
     if default == None:
         response = pyip.inputYesNo(prompt)
     else:
         response = pyip.inputYesNo(prompt, blank=True)
+
     if response == "":
         return default
     else:
         return response == "yes"
 
 if __name__ == '__main__':
-    while True:
+    for i in range(10):
         result = confirm("Confirm to submit this job?", default=True)
+        print(result)
